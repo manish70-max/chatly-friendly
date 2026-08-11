2f7434fb128f51991ae5ac172f04e31e080c867c
manish70-max <manishvarma7043@gmail.com>
2026-08-11 14:55:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 // const  app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatly-friendly.onrender.com",
     credentials:true
 }))
 app.use(express.json())
